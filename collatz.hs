0e6ff7757b97d23a87e1d5fb1f4b53c34353ad76
Matheus Portela <matheus.v.portela@gmail.com>
2016-08-09 00:16:28 -0300

    Finding number that generates the largest Collatz series.

--- collatz.hs
+++ collatz.hs
@@ -4,3 +4,13 @@ collatz x
     | otherwise = if (odd x)
                     then x : collatz (3*x + 1)
                     else x : collatz (x `div` 2)
+
+largestCollatz :: [Int] -> (Int, Int)
+largestCollatz xs = foldl
+                        (\ acc x ->
+                            let currentCollatzLength = length $ collatz x
+                                previousCollatzLength = (snd acc)
+                            in  if currentCollatzLength > previousCollatzLength
+                                        then (x, currentCollatzLength)
+                                        else acc)
+                        (0, 0) xs
