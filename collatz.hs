84b2d0b0f5a9303d3e4a8d481b6476d8f3464b00
Matheus Portela <matheus.v.portela@gmail.com>
2016-08-12 08:23:48 -0300

    Enhancing module structure.
    Now, we have different functions no calculate the maximum Collatz number
    and length. Also, `largestCollatz` returns automatically the largest
    Collatz sequence for all n < x.

--- collatz.hs
+++ collatz.hs
@@ -1,20 +1,31 @@
 collatz :: Int -> [Int]
-collatz x
-    | x == 1 = [1]
-    | otherwise = if (odd x)
-                    then x : collatz (3*x + 1)
-                    else x : collatz (x `div` 2)
-
-largestCollatz :: [Int] -> (Int, Int)
-largestCollatz xs = foldl
-                        (\ acc x ->
-                            let currentCollatzLength = length $ collatz x
-                                previousCollatzLength = (snd acc)
-                            in  if currentCollatzLength > previousCollatzLength
-                                        then (x, currentCollatzLength)
-                                        else acc)
-                        (0, 0) xs
+collatz 1 = [1]
+collatz x = if (odd x)
+                then x : collatz (3*x + 1)
+                else x : collatz (x `div` 2)
 
 toOne :: Int -> [Int]
 toOne 1 = [1]
 toOne x = x : toOne (x-1)
+
+seqToCollatzLength :: [Int] -> (Int, Int)
+seqToCollatzLength xs = foldl
+                        (\ acc x ->
+                            let currentLength = length $ collatz x
+                                previousLength = (snd acc)
+                            in  if currentLength > previousLength
+                                        then (x, currentLength)
+                                        else acc)
+                        (0, 0) xs
+
+largestCollatzNumAndLength :: Int -> (Int, Int)
+largestCollatzNumAndLength = seqToCollatzLength . toOne
+
+largestCollatzLength :: Int -> Int
+largestCollatzLength = snd . largestCollatzNumAndLength
+
+largestCollatzNum :: Int -> Int
+largestCollatzNum = fst . largestCollatzNumAndLength
+
+largestCollatz :: Int -> [Int]
+largestCollatz = collatz . largestCollatzNum
